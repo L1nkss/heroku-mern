121bfae0334d96a77e8d9bb75954be9520939a7c
Сергей Харлов <l1nks@inbox.ru>
2020-12-16 15:18:00 +0500

    'test'

--- index.ts
+++ index.ts
@@ -1,8 +1,13 @@
 import express from 'express';
 const app = express();
 const PORT = 8000;
+const router = express.Router();
 
-app.get('/', (req, res) => res.send('Express + Typescript Server'));
+router.get('/', (req, res) => {
+    console.log(__dirname)
+    res.send('Express + Typescript Server');
+})
+
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`)
